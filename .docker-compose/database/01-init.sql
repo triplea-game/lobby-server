0f19d6d907b6551ed3c234138ce394e718c7eb36
DanVanAtta <dhvatta@gmail.com>
2024-07-03 19:14:34 -0700

    Docker compose work - postgres launches with flyway migration

--- .docker-compose/database/01-init.sql
+++ .docker-compose/database/01-init.sql
@@ -1,5 +1,8 @@
-create user lobby_user password 'lobby';
-create database lobby_db owner lobby_user;
+create user lobby_user with password 'lobby';
+create user lobby_flyway with password 'lobby_flyway';
+create database lobby_db with owner lobby_flyway;
+grant all privileges on database lobby_db to lobby_user;
 
-create user error_report_user password 'error_report';
-create database error_report owner error_report_user;
+-- create user error_report_user password 'error_report';
+-- create database error_report owner error_report_user;
+
