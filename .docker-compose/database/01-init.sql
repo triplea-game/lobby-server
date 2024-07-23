725aafc2a6e3d296bd5f3e6a887fb8b7dfe2e4b8
DanVanAtta <dhvatta@gmail.com>
2024-07-23 09:46:42 -0700

    DB Connection config: clean up config, lobby_user needs to be owner of DB

--- .docker-compose/database/01-init.sql
+++ .docker-compose/database/01-init.sql
@@ -1,5 +1,3 @@
-create user lobby_user with password 'lobby';
-create user lobby_flyway with password 'lobby_flyway';
-create database lobby_db with owner lobby_flyway;
-grant all privileges on database lobby_db to lobby_user;
+create user lobby_user with password 'lobby_user';
+create database lobby_db with owner lobby_user;
 
