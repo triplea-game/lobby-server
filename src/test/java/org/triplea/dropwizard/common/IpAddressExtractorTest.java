43faed278207108c8863b9e0de2317cc1bae0c84
DanVanAtta <dhvatta@gmail.com>
2025-06-06 21:00:24 -0700

    Improve test cases around IP address extraction/parsing
    (1) Add some additional IPv6 variants
    
    (2) Avoid using '127.0.0.1' as the example IP address that is banned.
    Using the lobby after tests have run to debug is not uncommon, having
    localhost be banned is not ideal!

--- src/test/java/org/triplea/dropwizard/common/IpAddressExtractorTest.java
+++ src/test/java/org/triplea/dropwizard/common/IpAddressExtractorTest.java
@@ -5,43 +5,27 @@ import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class IpAddressExtractorTest {
 
-  private static final String IPV4 = "127.0.0.1";
-  private static final String IPV6 = "3ffe:1900:4545:3:200:f8ff:fe21:67cf";
-
   @Mock private HttpServletRequest httpServletRequest;
 
-  @Test
-  void ipv6() {
-    when(httpServletRequest.getRemoteAddr()).thenReturn(IPV6);
-    assertThat(
-        "Normal formatted IPv6 as input should return same value",
-        IpAddressExtractor.extractIpAddress(httpServletRequest),
-        is(IPV6));
-  }
-
-  @Test
-  void ipv6_WithBrackets() {
-    when(httpServletRequest.getRemoteAddr()).thenReturn("[" + IPV6 + "]");
-    assertThat(
-        "Square brackets on the IPv6 should be stripped",
-        IpAddressExtractor.extractIpAddress(httpServletRequest),
-        is(IPV6));
-  }
-
-  @Test
-  void ipv4() {
-    when(httpServletRequest.getRemoteAddr()).thenReturn(IPV4);
-    assertThat(
-        "IPv4 format is returned unaltered",
-        IpAddressExtractor.extractIpAddress(httpServletRequest),
-        is(IPV4));
+  @ParameterizedTest
+  @ValueSource(
+      strings = {
+        "127.0.0.1",
+        "3ffe:1900:4545:3:200:f8ff:fe21:67cf",
+        "[3ffe:1900:4545:3:200:f8ff:fe21:67cf]",
+        "2600:4041:5d8e:d900:a19e:b761:e274:7139",
+      })
+  void extract(String ip) {
+    when(httpServletRequest.getRemoteAddr()).thenReturn(ip);
+    assertThat(IpAddressExtractor.extractIpAddress(httpServletRequest), is(ip));
   }
 }
--- src/testInteg/java/org/triplea/db/dao/user/ban/UserBanDaoTest.java
+++ src/testInteg/java/org/triplea/db/dao/user/ban/UserBanDaoTest.java
@@ -30,18 +30,18 @@ class UserBanDaoTest {
   @DataSet(value = "user_ban/banned_by_ip.yml", useSequenceFiltering = false)
   class IsBannedByIp {
     @Test
-    void isBannedByIpPositiveCase() {
-      assertThat(userBanDao.isBannedByIp("127.0.0.1"), is(true));
-    }
-
-    @Test
-    void notBannedWhenIpNotPresent() {
+    void notBanned() {
       assertThat(userBanDao.isBannedByIp("1.1.1.1"), is(false));
     }
 
     @Test
-    void notBannedWhenBanIsExpired() {
-      assertThat(userBanDao.isBannedByIp("127.0.0.2"), is(false));
+    void isBanned() {
+      assertThat(userBanDao.isBannedByIp("127.0.0.2"), is(true));
+    }
+
+    @Test
+    void banExpired() {
+      assertThat(userBanDao.isBannedByIp("127.0.0.3"), is(false));
     }
   }
 
@@ -56,14 +56,14 @@ class UserBanDaoTest {
       assertThat(result, hasSize(2));
       assertThat(result.get(0).getBanExpiry(), isInstant(2100, 1, 1, 23, 59, 59));
       assertThat(result.get(0).getDateCreated(), isInstant(2020, 1, 1, 23, 59, 59));
-      assertThat(result.get(0).getIp(), is("127.0.0.2"));
+      assertThat(result.get(0).getIp(), is("127.0.0.3"));
       assertThat(result.get(0).getPublicBanId(), is("public-id2"));
       assertThat(result.get(0).getSystemId(), is("system-id2"));
       assertThat(result.get(0).getUsername(), is("username2"));
 
       assertThat(result.get(1).getBanExpiry(), isInstant(2200, 1, 1, 23, 59, 59));
       assertThat(result.get(1).getDateCreated(), isInstant(2010, 1, 1, 23, 59, 59));
-      assertThat(result.get(1).getIp(), is("127.0.0.1"));
+      assertThat(result.get(1).getIp(), is("127.0.0.2"));
       assertThat(result.get(1).getPublicBanId(), is("public-id1"));
       assertThat(result.get(1).getSystemId(), is("system-id1"));
       assertThat(result.get(1).getUsername(), is("username1"));
@@ -72,7 +72,9 @@ class UserBanDaoTest {
     @Test
     @DisplayName("Verify ban lookup case with no results found")
     void lookupBanEmptyCase() {
-      final Optional<BanLookupRecord> result = userBanDao.lookupBan("99.99.99.99", "system-id-DNE");
+
+      final Optional<BanLookupRecord> result =
+          userBanDao.lookupBan("2600:4041:5d8e:d900:a19e:b761:e274:7139", "system-id-DNE");
 
       assertThat(result, isEmpty());
     }
@@ -80,7 +82,7 @@ class UserBanDaoTest {
     @Test
     @DisplayName("Verify ban lookup by IP address")
     void lookupBanRecordByIp() {
-      final Optional<BanLookupRecord> result = userBanDao.lookupBan("127.0.0.2", "any-system-id");
+      final Optional<BanLookupRecord> result = userBanDao.lookupBan("127.0.0.3", "any-system-id");
       assertThat(
           result,
           isPresentAndIs(
