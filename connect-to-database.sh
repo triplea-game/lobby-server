eb16561b68c8c0576ad37e396771935350b93dcb
DanVanAtta <dhvatta@gmail.com>
2025-06-06 20:11:16 -0700

    connect-to-database.sh: connect to container running on port 5432
    Update the helper script to be a bit smarter, rather than assuming the
    name of database, find any docker container exposing the expected
    database port, 5432, and connect to that container with 'psql'

--- connect-to-database.sh
+++ connect-to-database.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker exec -it --user postgres lobby-database-1 psql
+
+# Look for any containers publishing port 5432, the port we expect postgres to be using
+dbContainerName=$(docker ps --filter publish=5432 --filter status=running --format {{.Names}})
+
+# Run 'psql' as 'postgres' user on the DB container
+docker exec -it --user postgres "$dbContainerName" psql lobby_db
 
