110f402153c35de161664e00a93eb95f76f35a85
Dan Van Atta <dhvatta@gmail.com>
2025-06-25 21:37:14 -0700

    Simplify verify.sh - assume that docker will be installed (#71)

--- verify.sh
+++ verify.sh
@@ -1,51 +1,5 @@
 #!/bin/bash
 
 # This script runs all checks across the entire project.
-
-# Checks if a dependency is installed on a system.
-function checkDependency {
-  local depName="$1"
-  if hash "$depName"; then
-    return 0
-  else
-    echo "ERROR: dependency not installed '$depName'"
-    return 1
-  fi
-}
-
-# Installs Docker and dependencies
-# Docker is installed through python, we install python & pip first, then docker.
-# Last, we add current user to the 'docker' group to enable sudo-less docker.
-function installDocker {
-  if [ "$(uname)" == "Darwin" ]; then
-    echo "Follow these steps to install docker: https://store.docker.com/editions/community/docker-ce-desktop-mac"
-    exit 1
-  fi
-  echo "Installing python and pip, a dependency of docker"
-  if hash yum; then
-    set -x
-    sudo yum update -y
-    sudo yum install -y python3 python3-pip
-    set +x
-  else
-    set -x
-    sudo apt install -y python3 python3-pip
-    set +x
-  fi
-  echo "Installing Docker (with pip)"
-  set -x
-  pip3 install docker docker-compose
-  set +x
-
-  echo "Adding current user: $USER to group: 'docker' (allows sudo-less docker)."
-  echo "Log back in for these changes to take effect."
-  groups | grep -q "docker" || sudo usermod -a -G docker "$USER"
-}
-
-scriptDir="$(dirname $0)"
-set -o pipefail
-set -eu
-
-checkDependency "docker" || installDocker
 "$scriptDir/gradlew" spotlessApply check $@
 
