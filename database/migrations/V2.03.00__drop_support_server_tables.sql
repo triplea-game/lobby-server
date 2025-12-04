3c5db30d173562f9a3f0ae09a9b70920451560d3
DanVanAtta <dhvatta@gmail.com>
2025-12-03 19:22:16 -0800

    Drop (maps) support server tables

---
+++ database/migrations/V2.03.00__drop_support_server_tables.sql
@@ -0,0 +1,10 @@
+/*
+    Tables below now belong to the "support server"
+    The support server has its own database, these tables
+    will be in that database & can be removed from the lobby_db.
+ */
+drop table lobby_message;
+drop table map_tag_value;
+drop table map_tag_allowed_value;
+drop table map_tag;
+drop table map_index;
